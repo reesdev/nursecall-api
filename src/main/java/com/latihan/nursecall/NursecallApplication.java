1844ae705bd347042ef8550fc0b5b8d73d8e51fa
Riski <reesky.it@gmail.com>
2026-03-13 22:58:40 +0700

    init nurse call system API

---
+++ src/main/java/com/latihan/nursecall/NursecallApplication.java
@@ -0,0 +1,13 @@
+package com.latihan.nursecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursecallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursecallApplication.class, args);
+	}
+
+}
